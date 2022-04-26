137284c7dfb12b9bc6a0d49a7d4d713bc4c8bd3c
ehsan <savvyversa@gmail.com>
2022-04-26 16:53:49 +0430

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+import {defineNuxtConfig} from 'nuxt'
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    modules: [
+        // Using package name
+        // '@nuxtjs/content',
+        // Relative to your project srcDir
+        // '~/modules/vsd',
+        '~/modules/vsp',
+        // 'vuetify-strapi-page',
+        // Providing options
+    ]
+})
